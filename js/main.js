55d71c7572f1bb2c8a2e2cb8cde7d3c0b81a2de4
sodiqovsodiqjon <sodiqjon191929@gmail.com>
2023-02-25 13:28:10 +0500

    push

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+
+// DARC mode
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+
+
+
+
+// var splide = new Splide( '.splide', {
+//      type  : 'fade',
+//      rewind: true,
+//    } );
+   
+//    splide.mount();
+
+
+// let box = document.querySelector('.box')
+
+// fetch('https://restcountries.com/v3.1/all')
+//      .then((res) => res.json())
+//      .then((data) =>{
+//           for(let i = 0; i< data.length; i++){
+//                box.innerHTML += `
+//                <p>${data[i].name.common}</p>
+//         <img src="${data[i].flags.png}" alt="">
+    
+//                `
+//      }
+//      }
+// )
+
+
+
+
+
+
+
+
+
+
+
+
